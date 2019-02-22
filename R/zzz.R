563f4dacf29d5dbf071097a311286c234c3601e3
Aaron <achafetz@usaid.gov>
2019-02-21 20:52:13 -0500

    identify global variables

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.onLoad <- function (libname, pkgname)
+{
+  # make data set names global to avoid CHECK notes
+  utils::globalVariables ("amt")
+  utils::globalVariables ("amt_type")
+  utils::globalVariables ("beneficiary")
+  utils::globalVariables ("cop")
+  utils::globalVariables ("path")
+  utils::globalVariables ("program")
+  utils::globalVariables ("programarea")
+
+  invisible ()
+}

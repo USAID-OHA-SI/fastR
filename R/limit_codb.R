8e4b53d45061aa3be552ceeff3dc6883441568f5
Aaron Chafetz <achafetz@usaid.gov>
2019-03-05 08:48:25 -0500

    fix issue where HHS/SAMSTA does not have TOTAL in the name in Vietnam and was previously included

--- R/limit_codb.R
+++ R/limit_codb.R
@@ -12,7 +12,7 @@ limit_codb <- function(df){
 
   #filter out missing and aggregated rows & drop any without an codb total
   df <- df %>%
-    dplyr::filter(!stringr::str_detect(costtype, "COP|Cycle|TOTAL"),
+    dplyr::filter(!stringr::str_detect(costtype, "COP|Cycle|TOTAL|HHS"),
                   total_codb_drop > 0)
 
   #drop no essential columns

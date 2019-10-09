160bf7fe2efe28f5dcdfbe701c767ab85350784b
Aaron Chafetz <achafetz@usaid.gov>
2019-10-09 17:07:12 -0400

    adj to keep initiatives and change 0s to NAs

--- R/gather_cross.R
+++ R/gather_cross.R
@@ -19,14 +19,25 @@ gather_cross <- function(df, filepath){
 
   #combine iniatives into type column
   if(is_oldformat(filepath)) {
-    df <- dplyr::mutate(df, initiative_type = dplyr::coalesce(initiative1, initiative2))
+    #df <- dplyr::mutate(df, initiative_type = dplyr::coalesce(initiative2, initiative1))
+    df <- df %>%
+      dplyr::mutate_at(dplyr::vars(initiative1, initiative2), ~ na.if(., 0)) %>%
+      tidyr::unite(initiative_type, initiative1, initiative2, sep = "/") %>%
+      dplyr::mutate(initiative_type = stringr::str_remove_all(initiative_type, "/NA"),
+                    initiative_type = stringr::str_remove_all(initiative_type, "Bilateral 19/"))
   } else {
-    df <- dplyr::mutate(df, initiative_type = dplyr::coalesce(initiative1, initiative2, initiative3))
+    #df <- dplyr::mutate(df, initiative_type = dplyr::coalesce(initiative3, initiative2, initiative1))
+    df <- df %>%
+      dplyr::mutate_at(dplyr::vars(initiative1, initiative2, initiative3), ~ na.if(., 0)) %>%
+      tidyr::unite(initiative_type, initiative1, initiative2, initiative3, sep = "/") %>%
+      dplyr::mutate(initiative_type = stringr::str_remove_all(initiative_type, "/NA"),
+                    initiative_type = stringr::str_remove_all(initiative_type, "Bilateral 19/"))
+
   }
 
   #remove initiative columns and references in budget code
   df <- df %>%
-    dplyr::select(-dplyr::starts_with("initiative")) %>%
+    #dplyr::select(-dplyr::starts_with("initiative")) %>%
     dplyr::mutate(init_type = stringr::str_remove(alloc_type, "bilat_|init2_|init3_"))
 
 
